fe19e9cb7543c1485b7d77834959333dbf185b48
John Mai <john.mai@deluxe.com>
2014-11-18 10:04:38 -0500

    add user following

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module SampleApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
+    # Include the authenticity token in remote forms.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
